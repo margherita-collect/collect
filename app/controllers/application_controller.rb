8aa9e619f4a317f80249d5429b79177870a31d2c
c-04 <49675227+c-04@users.noreply.github.com>
2019-07-07 18:27:50 +0900

    Revert "device作成"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-
-before_action :configure_permitted_parameters, if: :devise_controller?
-
-
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:surname, :first_name, :kana_surname,:kana_first_name,:phone_number,:zip_code,:address,:status])
-  end
-
 end
