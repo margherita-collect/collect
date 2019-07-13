c045799ce98523df83775da8e7847d1341da0e76
maiki <ohishi.maiki.infratop@gmail.com>
2019-07-13 15:30:48 +0900

    管理者モデル&ビューの作成、routes.rbの作業進行

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
 
-
-  resources :products, only: [:show, :index]
+  devise_for :admins
   devise_for :users
 
-  get 'products/index'
-  get 'products/show'
+  resources :products, only: [:show, :index]
+  resources :users, only: [:edit, :update, :show] do
+  	resource :carts, only: [:create, :destroy, :index]
+  	resource :purchases, only: [:index]
+  end
 
+  namespace :admin do
+  	resources :products, only: [:new, :create, :edit, :update, :index, :show]
+  	resources :purchases, only: [:index, :update]
+  	resources :users, only: [:index, :show, :edit, :update]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_07_13_045509) do
 
-ActiveRecord::Schema.define(version: 2019_07_07_090827) do
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "artist_name"
@@ -33,8 +44,6 @@ ActiveRecord::Schema.define(version: 2019_07_07_090827) do
     t.datetime "updated_at", null: false
   end
 
-ActiveRecord::Schema.define(version: 2019_07_07_050447) do
-
   create_table "products", force: :cascade do |t|
     t.string "image_id"
     t.string "product_name"
@@ -48,13 +57,13 @@ ActiveRecord::Schema.define(version: 2019_07_07_050447) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "songs", force: :cascade do |t|
     t.integer "disc_id"
     t.string "song_name"
     t.integer "song_order"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -77,7 +86,7 @@ ActiveRecord::Schema.define(version: 2019_07_07_050447) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
