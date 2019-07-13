7a89be3a9e8a95856a87e78c344b87cbcae1cd76
maiki <ohishi.maiki.infratop@gmail.com>
2019-07-13 18:09:39 +0900

    routes.rbの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
 
-
-  devise_for :admins
-  resources :products, only: [:show, :index]
   devise_for :users
+  devise_for :admins
 
-  get 'products/index'
-  get 'products/show'
+  resources :products, only: [:show, :index]
+  resources :users, only:[:show, :edit, :update] do
+  	resource :purchases, only: [:index]
+  	resource :carts, only: [:create, :destroy, :index]
+  end
 
+  namespace :admin do
+  	resources :products, only: [:new, :create, :edit, :update, :index, :show]
+  	resources :purchases, only: [:index, :update]
+  	resources :users, only: [:index, :show, :edit, :update]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
