edf00e9cc8dafc054e8a3a4259dc5a55aba6e4c0
maiki <ohishi.maiki.infratop@gmail.com>
2019-07-13 18:45:19 +0900

    ビューとコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'purchases/index'
+  end
+  namespace :admin do
+    get 'products/new'
+    get 'products/edit'
+    get 'products/index'
+    get 'products/show'
+  end
+  get 'carts/index'
+  get 'purchases/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   devise_for :admins
 
