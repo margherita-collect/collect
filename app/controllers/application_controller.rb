6c88ff4b54b6026fa3efb4085c6c56b049d01396
c-04 <papukyulitos@gmail.com>
2019-07-16 08:58:18 +0000

    機能作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,9 @@ class ApplicationController < ActionController::Base
 	#ログイン認証済みのユーザーにのみページを表示する
 	before_action :authenticate_user!
 
+
 	protected
 	def configure_permitted_parameters
-    	devise_parameter_sanitizer.permit(:sign_up, keys: [:surname, :first_name, :kana_surname,:kana_first_name,:phone_number,:zip_code,:address,:status])
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:surname, :firstname, :kana_surname,:kana_first_name,:phone_number,:zip_code,:address,:status])
   	end
 end
