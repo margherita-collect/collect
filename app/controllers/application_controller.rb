d7f8a1e723321d8ffbf3f228521d1b72913de3a0
c-04 <papukyulitos@gmail.com>
2019-07-20 10:12:40 +0000

    pull

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,22 @@ class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
 
 	def after_sign_in_path_for(resource)
-  		admin_products_path
+			case resource
+			when Admin
+  					admin_products_path
+			when User
+		    		root_path
+			end
 	end
 
+
 	def after_sign_out_path_for(resource)
-  		admin_products_path
+		case resource
+		when Admin
+					admin_products_path
+		when User
+					root_path
+		end
 	end
 
 	protected
